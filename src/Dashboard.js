b571445d344a6db465a0d5a467769e6e5ca406b1
chrisdews <cldews1@gmail.com>
2020-04-24 14:56:58 +0100

    Chat window with material-ui core.

---
+++ src/Dashboard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2)
+  }
+}));
+
+const Dashboard = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <Typography variant="h5" component="h3">
+          This is a sheet of paper
+        </Typography>
+        <Typography component="p">Paragraph</Typography>
+      </Paper>
+    </div>
+  );
+};
+
+export default Dashboard;
